4fc33820bba3bddb01532b786fc60a89a35abf7e
Sam Jaques <sam.jaques@me.com>
2022-12-26 15:33:37 +0100

    feat: Stage 3 - Step 2: main

--- 4. Projects/1. CLI/Problem/Stage 3/Step 2/src/main.rs
+++ 4. Projects/1. CLI/Problem/Stage 3/Step 2/src/main.rs
@@ -3,27 +3,47 @@ use std::rc::Rc;
 mod models;
 
 mod db;
-use db::*;
-
-mod ui;
 
 mod io_utils;
-use io_utils::*;
+mod ui;
 
 mod navigator;
-use navigator::*;
+use navigator::Navigator;
 
 fn main() {
-    // TODO: create database and navigator
-    
+    let db = Rc::new(db::JiraDatabase::new("./data/db.json".to_owned()));
+    let mut navigator = Navigator::new(Rc::clone(&db));
+
     loop {
         clearscreen::clear().unwrap();
 
-        // TODO: implement the following functionality:
-        // 1. get current page from navigator. If there is no current page exit the loop.
-        // 2. render page
-        // 3. get user input
-        // 4. pass input to page's input handler
-        // 5. if the page's input handler returns an action let the navigator process the action
+        let page = match navigator.get_current_page() {
+            None => break,
+            Some(page) => page,
+        };
+
+        if let Err(error) = page.draw_page() {
+            println!("Error rendering page: {error}\nPress any key to continue...");
+            io_utils::wait_for_key_press();
+            continue;
+        };
+
+        let input = io_utils::get_user_input();
+
+        let action = match page.handle_input(&input.trim()) {
+            Err(error) => {
+                println!("Failed to handle input: {error}\nPress any key to continue...");
+                io_utils::wait_for_key_press();
+                continue;
+            }
+            Ok(action) => action,
+        };
+
+        if let Some(action) = action {
+            if let Err(error) = navigator.handle_action(action) {
+                println!("Error handling action: {error}\nPress any key to continue...");
+                io_utils::wait_for_key_press();
+            };
+        }
     }
 }
